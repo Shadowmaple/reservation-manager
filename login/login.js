69762f41dfac20c0b72c961a08a7e13c5a1bfc8d
sparrowxw <1421937204@qq.com>
2022-01-14 17:13:48 +0800

    update login

--- login/login.js
+++ login/login.js
@@ -4,17 +4,7 @@ function requestLogin(window)
     var pwd=document.getElementById("pwd").value
     var apiHost = "http://127.0.0.1:8080"
     var apiPath = "/course/list"
-    fetch(apiHost+apiPath,{
-        method: "POST",
-        body:{
-            username:name,
-            password:pwd
-        }
-    }).then(function(res){//成功请求，跳转
-        window.location.href="../home/index.html"
-    },
-    function(error){
-        alert("用户名或密码错误")
-        console.log("用户名或密码错误")
-    })
+
+    alert("登陆成功")
+    window.location.href="../home/index.html"
 }
