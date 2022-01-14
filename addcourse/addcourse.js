dac3e1c1445f1353d4390fc1a6f92675a8f8a035
sparrowxw <1421937204@qq.com>
2022-01-14 14:16:39 +0800

    update login addcourse

---
+++ addcourse/addcourse.js
@@ -0,0 +1,46 @@
+function clickback(){
+    history.back();
+}
+
+
+function sendclick(){
+    var url = "http://127.0.0.1:8080/course"
+    var course_name = document.getElementById("course_name").value
+    var teacher_name = document.getElementById("teacher_name").value
+    var restrict_number = document.getElementById("restrict_number").value
+    var start_time = document.getElementById("start_time").value
+    var end_time = document.getElementById("end_time").value
+    var imgurl = document.getElementById("imgurl").value
+    var course_point = document.getElementById("course_point").value
+    var period = document.getElementById("period").value
+    var intro = document.getElementById("intro").value
+    var data = {
+        "name":course_name,
+        "teacher":teacher_name,
+        "desc":intro,
+        "total":restrict_number,
+        "image":imgurl,
+        "price":course_point,
+        "start_time":start_time,
+        "end_time":end_time,
+        "period":period
+    }
+    
+    fetch(url, {
+        body: JSON.stringify(data), // must match 'Content-Type' header
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, same-origin, *omit
+        headers: {
+          'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+          'content-type': 'application/json'
+        },
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // *client, no-referrer
+      }).then(Response=>{
+        alert("发布课程成功");
+        window.location.href="../home/index.html"
+      }).catch(error=>console.log("request fail"))
+    
+}
--- login/login.js
+++ login/login.js
@@ -4,16 +4,17 @@ function requestLogin(window)
     var pwd=document.getElementById("pwd").value
     var apiHost = "http://127.0.0.1:8080"
     var apiPath = "/course/list"
-    window.fetch(apiHost+apiPath,{
-        methods: "POST",
+    fetch(apiHost+apiPath,{
+        method: "POST",
         body:{
             username:name,
             password:pwd
         }
     }).then(function(res){//成功请求，跳转
-        window.open("../index.html")
+        window.location.href="../home/index.html"
     },
     function(error){
+        alert("用户名或密码错误")
         console.log("用户名或密码错误")
     })
 }
